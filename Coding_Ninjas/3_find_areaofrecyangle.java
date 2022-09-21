166247ca9aff66f516597d4f988f3e717f74eb44
Shruti Srivastava <shrutishreya222@gmail.com>
2022-09-21 22:08:10 +0530

    prog3

---
+++ Coding_Ninjas/3_find_areaofrecyangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+        Scanner sc= new Scanner(System.in);
+        int length=sc.nextInt();
+        int breadth=sc.nextInt();
+        int area = (length*breadth);
+        System.out.println(area);
+		
+	}
+}
