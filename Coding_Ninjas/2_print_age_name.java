a2fdaa8924b0ec91c971a9ced5afd315558a187e
Shruti Srivastava <shrutishreya222@gmail.com>
2022-09-21 22:07:07 +0530

    prog 2

---
+++ Coding_Ninjas/2_print_age_name.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+        Scanner sc= new Scanner(System.in);
+        String Name=sc.nextLine();
+        int Age=sc.nextInt();
+        System.out.println("The name of the person is "+Name+" and the age is "+Age+".");
+        
+		
+	}
+}
