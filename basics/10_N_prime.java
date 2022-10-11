163891f372bbb0f6c94960272a129ce571b0e53f
Shruti Srivastava <shrutishreya222@gmail.com>
2022-10-11 22:41:25 +0530

    prog 10

---
+++ basics/10_N_prime.java
@@ -0,0 +1,23 @@
+public import java.util.Scanner;
+
+public class primeN {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+
+        for(int n = low; n <= high; n++){
+            int count=0;
+            for(int div=2 ; div * div <=n; div++){
+                if(n % div==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println(n);
+            }
+        }
+        System.out.println("only prime no. are printed");
+    }
+}
